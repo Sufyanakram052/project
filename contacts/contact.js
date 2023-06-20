573734da460c7fbf865213644eb66ded1c877e38
Software Developer <fe.developer@smartdigital.com.sa>
2023-06-20 05:20:24 +0500

    wip

--- contacts/contact.js
+++ contacts/contact.js
@@ -31,10 +31,22 @@ router.get('/:id', (req, res) => {
     }
     res.render(path.join(__dirname, '../views/contactinfo.pug') ,  {title: 'P2 Contacts', rows})
   });
-  //res.render(path.join(__dirname, '../views/contactinfo.pug') ,  {title: 'P2 Contacts'})
-
 });
 
+//Contact Info
+router.get('/:id/edit', (req, res) => {
+  const formData = req.params.id;
+  const sql = `SELECT * FROM Contacts where id = ${formData}`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    res.render(path.join(__dirname, '../views/updatecontact.pug') ,  {title: 'P2 Contacts', rows})
+  });
+});
+
+
+//Create
 router.post('/creates', (req, res) => {
     const formData = req.body;
 
@@ -75,46 +87,76 @@ router.post('/creates', (req, res) => {
     
 });
 
+//Update
+router.post('/edit', (req, res) => {
+  const contactId = parseInt(req.body.ID);
+  const formData = req.body;
+
+  db.run(
+    `UPDATE Contacts SET
+      FirstName = ?,
+      LastName = ?,
+      PhoneNumber = ?,
+      EmailAddress = ?,
+      Street = ?,
+      City = ?,
+      State = ?,
+      Zip = ?,
+      Country = ?,
+      Contact_By_Email = ?,
+      Contact_By_Phone = ?
+    WHERE ID = ?`,
+    [
+      formData.firstName,
+      formData.lastName,
+      formData.phoneNumber,
+      formData.emailAddress,
+      formData.street,
+      formData.city,
+      formData.state,
+      formData.zip,
+      formData.country,
+      formData.contactByEmail ? 1 : 0,
+      formData.contactByPhone ? 1 : 0,
+      contactId
+    ],
+    function (err) {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Error updating data in the database.');
+      } else {
+        res.redirect('/');
+      }
+    }
+  );
+});
+
+//Delete
+router.get('/:id/delete', (req, res) => {
+  const contactId = req.params.id;
+  const sql = `DELETE FROM Contacts WHERE ID = ?`;
+  
+  db.run(sql, [contactId], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Error deleting data from the database.');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+router.get('/delete', (req, res) => {
+  const contactId = req.params.id;
+  const sql = `DELETE FROM Contacts WHERE ID = ?`;
+  
+  db.run(sql, [contactId], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Error deleting data from the database.');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 
 module.exports = router;
-
-// const createContact = (contact) => {
-//     return new Promise((resolve, reject) => {
-//       const sql = `
-//         INSERT INTO Contacts (
-//           FirstName,
-//           LastName,
-//           PhoneNumber,
-//           EmailAddress,
-//           Street,
-//           City,
-//           State,
-//           Zip,
-//           Country,
-//           Contact_By_Email,
-//           Contact_By_Phone
-//         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-//       `;
-//       const values = [
-//         contact.firstName,
-//         contact.lastName,
-//         contact.phoneNumber,
-//         contact.emailAddress,
-//         contact.street,
-//         contact.city,
-//         contact.state,
-//         contact.zip,
-//         contact.country,
-//         contact.contactByEmail,
-//         contact.contactByPhone
-//       ];
-  
-//       db.run(sql, values, function (err) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(this.lastID);
-//         }
-//       });
-//     });
-//   };
